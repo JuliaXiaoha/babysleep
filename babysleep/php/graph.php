5ad47693876ea89cb2c40b38949a3ed62a5fbb27
JuliaXiaoha <glister888@hotmail.com>
2013-07-09 13:03:42 -0400

    baby graph.php

--- babysleep/php/graph.php
+++ babysleep/php/graph.php
@@ -6,15 +6,24 @@ header("Content-type:image/png");
 $myImage = ImageCreate(300,300);
 
 // the first color allocated is the background color
-$black = ImageColorAllocate($myImage,0,0,0);
 $white = ImageColorAllocate($myImage,0xff,0xff,0xff);
 $red = ImageColorAllocate($myImage,0xff,0,0);
-$blue =ImageColorAllocate($myImage,0,0,0xff);
 $green = ImageColorAllocate($myImage,0,0xff,0);
 
-ImageFilledArc($myImage, 110, 110, 200,150, 0,90, $red,IMG_ARC_PIE);
-ImageFilledArc($myImage, 100, 100, 200,150, 90,180, $green,IMG_ARC_PIE);
-ImageFilledArc($myImage, 100, 100, 200,150, 180,360, $blue,IMG_ARC_PIE);
+
+//Draw a green rectangle around the edge of the screen
+
+ImageRectangle($myImage,0,0,299,199,$green);
+
+// Start Drawing line on graph
+
+ImageLine($myImage,10,40,50,10,$green);
+ImageLine($myImage,50,10,90,60,$green);
+ImageLine($myImage,90,60,130,30,$green);
+ImageLine($myImage,130,30,170,70,$green);
+ImageLine($myImage,170,70,210,40,$green);
+ImageLine($myImage,210,40,250,50,$green);
+
 // send the bytes of the image
 ImagePng($myImage);
 
